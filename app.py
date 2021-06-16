40b213a60c9d1c91b8602b0990d4536024c934a1
Pooja-mana <85435997+Pooja-mana@users.noreply.github.com>
2021-06-17 02:01:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -556,7 +556,7 @@ def webhook():
 
 # run the app
 if __name__ == '__main__':
-   port = int(os.getenv('PORT',8080))
+   port = int(os.getenv('PORT',5000))
    app.run(host='0.0.0.0', port=port, debug = false)
    
 
